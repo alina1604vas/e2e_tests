911e0b6d5bbf61464a06a7bec1314b01fd82b197
Alina_Rubanska <alina1604vas@gmail.com>
2024-12-24 14:53:58 +0200

    Changed structure. Login, check, search.

--- src/test/java/io/testomat/e2e_tests/ProjectPageTests.java
+++ src/test/java/io/testomat/e2e_tests/ProjectPageTests.java
@@ -1,35 +1,60 @@
 package io.testomat.e2e_tests;
 
 import com.codeborne.selenide.*;
-import com.codeborne.selenide.selector.ByText;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ProjectPageTests {
 
+    private final Dotenv dotenv = Dotenv.load();
+
     @Test
+    public void test1() {
+        login();
+        checkElements();
+        doSearch();
+    }
+
     public void login() {
-        Configuration.baseUrl = "https://app.testomat.io";
-        open("/");
+        String envBaseUrl = dotenv.get("BASE_URL");
+        open(envBaseUrl);
+
+        String envUsername = dotenv.get("USER_NAME");
+        String envPassword = dotenv.get("PASSWORD");
 
         SelenideElement email = $("#content-desktop #user_email");
         SelenideElement password = $("#content-desktop #user_password");
         SelenideElement rememberMe = $("#content-desktop #user_remember_me");
         SelenideElement signInButton = $x("//div[@id=\"content-desktop\"]//input[@value=\"Sign In\"]");
 
-        email.setValue("alina1604vas@gmail.com");
-        password.setValue("Manufacture!123");
+        email.setValue(envUsername);
+        password.setValue(envPassword);
 
         rememberMe.click();
         signInButton.click();
+    }
 
+    public void checkElements() {
         Selenide.$(".common-flash-success").shouldBe(Condition.visible);
 
+        SelenideElement dropdown = $("#company_id.max-w-lg");
+        dropdown.click();
+
+        ElementsCollection options = dropdown.getOptions();
+        options.shouldHave(exactTexts("Free Projects", "QA Club Lviv"));
+
+        dropdown.selectOption("QA Club Lviv");
+
+        SelenideElement rightSideText = $x("//*[@id=\"container\"]/div[2]/div[1]/span/span");
+        rightSideText.shouldHave(text("Enterprise plan"));
+    }
+
+    public void doSearch() {
         SelenideElement searchField = $("#search");
         SelenideElement searchValue = searchField.setValue("manufacture light");
 
